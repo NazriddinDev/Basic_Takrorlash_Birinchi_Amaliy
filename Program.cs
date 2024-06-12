ec255fb7631c02ba4e9cd8c0daa3de028a15b75b
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2024-06-12 23:14:38 +0500

    INFRA:  Array ichidagi eng katta va eng kichik qiymatlarni ko’paytmasi

--- Program.cs
+++ Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
+using System.Linq;
 
-// 1 dan 1000 gacha toq sonlar yig'indisini hisoblash
-int n, k=1, S=0;
-Console.Write("Sonni kiriting => ");
-n=Convert.ToInt32(Console.ReadLine());
-while(k<=n)
+// // 1 dan 1000 gacha toq sonlar yig'indisini hisoblash
+// int n, k=1, S=0;
+// Console.Write("Sonni kiriting => ");
+// n=Convert.ToInt32(Console.ReadLine());
+// while(k<=n)
+// {
+//     if(k%2!=0)
+//     {
+//         S=S+k;
+//     }
+//     k++;
+// }
+// Console.WriteLine(S);
+
+// 2 Array ichidagi eng katta va eng kichik qiymatlarni ko’paytmasini ekranga chiqaradigan dastur
+int m, S;
+Console.Write("Array elementlari sonini kiriting => ");
+m=int.Parse(Console.ReadLine());
+int[] n= new int[m];
+
+for(int i=0; i<n.Length; i++)
 {
-    if(k%2!=0)
-    {
-        S=S+k;
-    }
-    k++;
+    Console.WriteLine("Array elementlarini kiriting => ");
+    n[i]=int.Parse(Console.ReadLine());
 }
-Console.WriteLine(S);
+
+S=n.Max()*n.Min();
+Console.WriteLine($" Arrayning katta va kichik elementlarining ko'paytmasi => {S}");
