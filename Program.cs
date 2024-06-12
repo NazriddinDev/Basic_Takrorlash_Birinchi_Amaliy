9c946af951f7edd4d2e928afbcdfaaf449630c59
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2024-06-12 19:20:34 +0500

    INFRA: Toq sonlar yig'indisi

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// 1 dan 1000 gacha toq sonlar yig'indisini hisoblash
+int n, k=1, S=0;
+Console.Write("Sonni kiriting => ");
+n=Convert.ToInt32(Console.ReadLine());
+while(k<=n)
+{
+    if(k%2!=0)
+    {
+        S=S+k;
+    }
+    k++;
+}
+Console.WriteLine(S);
